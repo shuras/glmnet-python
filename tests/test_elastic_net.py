518204aa98c503e5575baa8b2657311239986c79
madrury <matthew.drury.83@gmail.com>
2014-11-29 19:23:31 -0800

    Add examples of correct use in validate method tests.

--- tests/test_elastic_net.py
+++ tests/test_elastic_net.py
@@ -139,96 +139,152 @@ class TestElasticNet(unittest.TestCase):
                 )
 
     def test_validate_matrix(self):
+        '''Test the _validate_matrix method.'''
         Xdn = np.random.random(size=(50,10))
-        w = np.random.random(size=(10,))
-        y = np.dot(Xdn, w)
         enet = ElasticNet(alpha=.5)
+        # Invalid use:
+        #   Passing in a sparse matrix in the incorrect format.
         with self.assertRaises(ValueError):
             Xsp = csr_matrix(Xdn)
-            enet.fit(Xsp, y)
+            enet._validate_matrix(Xsp)
+        # Valid use:
+        #   Passing in a matrix in compressed sparse column format.
+        Xsp = csc_matrix(Xdn)
+        enet._validate_matrix(Xsp)
 
     def test_validate_inputs(self):
         X = np.random.random(size=(50,10))
         w = np.random.random(size=(10,))
-        y = np.dot(X, w)
         enet = ElasticNet(alpha=.5)
+        # Invalid Use:
+        #    Passing in a y that is too short.
         with self.assertRaises(ValueError):
-            yprime = y.copy()[:49]
-            enet.fit(X, yprime)
+            yprime = np.random.random(size=(49,))
+            enet._validate_inputs(X, yprime)
+        # Invalid use:
+        #    Passing in a y that matches the wrong dimenstion of X.
         with self.assertRaises(ValueError):
             yprime = np.random.random(size=(10,))
-            enet.fit(X, yprime)
+            enet._validate_inputs(X, yprime)
+        # Valid Use:
+        #    Passing in a y of the correct dimension.
+        yprime = np.random.random(size=(50,))
+        enet._validate_inputs(X, yprime)
 
     def test_validate_weights(self):
         X = np.random.random(size=(50,10))
         w = np.random.random(size=(10,))
         y = np.dot(X, w)
         enet = ElasticNet(alpha=.5)
+        # Invalid use
+        #    Passing in a sample weight vector that is too short.
         with self.assertRaises(ValueError):
             sw = np.ones(shape=(49,))
-            enet.fit(X, y, weights=sw)
+            enet._validate_weights(X, y, weights=sw)
+        # Invalid use:
+        #    Passing in a weight vector that matches the wrong dimenstion of X.
         with self.assertRaises(ValueError):
             sw = np.ones(shape=(10,))
-            enet.fit(X, y, weights=sw)
+            enet._validate_weights(X, y, weights=sw)
+        # Invalid use:
+        #    Passing in a weight vector containing a negative entry. 
         with self.assertRaises(ValueError):
             sw = np.ones(shape=(50,))
             sw[25] = -1
-            enet.fit(X, y, weights=sw)
+            enet._validate_weights(X, y, weights=sw)
+        # Valid Use:
+        #    Weight vector of the correct dimension with all non-negative 
+        #    entries.
+        sw = np.ones(shape=(50,))
+        enet._validate_weights(X, y, weights=sw)
 
     def test_validate_rel_penalties(self):
         X = np.random.random(size=(50,10))
         w = np.random.random(size=(10,))
         y = np.dot(X, w)
         enet = ElasticNet(alpha=.5)
+        # Invalid use
+        #    Passing in a rel_penalties vector that is too short.
         with self.assertRaises(ValueError):
             rel_pens = np.ones(shape=(9,))
-            enet.fit(X, y, rel_penalties=rel_pens)
+            enet._validate_rel_penalties(X, y, rel_penalties=rel_pens)
+        # Invalid use:
+        #    Passing in a rel_penalties that matches the wrong dimenstion of X.
         with self.assertRaises(ValueError):
             rel_pens = np.ones(shape=(50,))
-            enet.fit(X, y, rel_penalties=rel_pens)
+            enet._validate_rel_penalties(X, y, rel_penalties=rel_pens)
+        # Invalid use:
+        #    Passing in a rel_penalties containing a negative entry. 
         with self.assertRaises(ValueError):
             rel_pens = np.ones(shape=(10,))
             rel_pens[5] = -1
-            enet.fit(X, y, rel_penalties=rel_pens)
+            enet._validate_rel_penalties(X, y, rel_penalties=rel_pens)
+        # Valid use:
+        #    Rel_penalties has the correct dimenstion with all non-negative
+        #    entries.
+        rel_pens = np.ones(shape=(10,))
+        rel_pens[5] = 0 
+        enet._validate_rel_penalties(X, y, rel_penalties=rel_pens)
 
     def test_validate_excl_preds(self):
         X = np.random.random(size=(50,10))
         w = np.random.random(size=(10,))
         y = np.dot(X, w)
         enet = ElasticNet(alpha=.5)
+        # Invalid use
+        #    Passing in a excl_preds array that is to long.
         with self.assertRaises(ValueError):
             excl_preds = np.ones(shape=(12,))
-            enet.fit(X, y, excl_preds=excl_preds)
-        with self.assertRaises(ValueError):
-            excl_preds = np.ones(shape=(50,))
-            enet.fit(X, y, excl_preds=excl_preds)
+            enet._validate_excl_preds(X, y, excl_preds=excl_preds)
+        # Invalid use
+        #    Alltempt to exclude a predictor out of range, i.e. that does
+        #    not exist.
         with self.assertRaises(ValueError):
             excl_preds = np.ones(shape=(11,))
             excl_preds[0] = 1
             excl_preds[5] = 10
-            enet.fit(X, y, excl_preds=excl_preds)
+            enet._validate_excl_preds(X, y, excl_preds=excl_preds)
+        # Valid use 
+        #    Exclude some in range predictors.
+        excl_preds = np.array([1, 2, 4, 6, 8])
+        enet._validate_excl_preds(X, y, excl_preds=excl_preds)
 
     def test_validate_box_constraints(self):
         X = np.random.random(size=(50,10))
         w = np.random.random(size=(10,))
         y = np.dot(X, w)
         enet = ElasticNet(alpha=.5)
+        # Invalid use
+        #   Second dimension matches incorrect dimension of X.
         with self.assertRaises(ValueError):
            box_const = np.empty(shape=(2,50))
-           enet.fit(X, y, box_constraints=box_const)
-        with self.assertRaises(ValueError):
-           box_const = np.empty(shape=(10,2))
-           enet.fit(X, y, box_constraints=box_const)
+           enet._validate_box_constraints(X, y, box_constraints=box_const)
+        # Invalid use
+        #    Incorrect first dimension.
         with self.assertRaises(ValueError):
            box_const = np.empty(shape=(1,10))
-           enet.fit(X, y, box_constraints=box_const)
+           enet._validate_box_constraints(X, y, box_constraints=box_const)
+        # Invalid use
+        #    Transpose of a correct use.
+        with self.assertRaises(ValueError):
+           box_const = np.empty(shape=(10,2))
+           enet._validate_box_constraints(X, y, box_constraints=box_const)
+        # Invalid use
+        #    Positive lower bound in constraint.
         with self.assertRaises(ValueError):
            box_const = np.empty(shape=(2,10))
            box_const[0,:] = -1; box_const[1,:] = 1
            box_const[0,5] = 1
-           enet.fit(X, y, box_constraints=box_const)
+           enet._validate_box_constraints(X, y, box_constraints=box_const)
+        # Invalid use
+        #    Negative upper bound in constraint.
         with self.assertRaises(ValueError):
            box_const = np.empty(shape=(2,10))
            box_const[0,:] = -1; box_const[1,:] = 1
            box_const[1,5] = -1
-           enet.fit(X, y, box_constraints=box_const)
+           enet._validate_box_constraints(X, y, box_constraints=box_const)
+        # Valid use
+        #     Impose some box constraints.
+        box_const = np.empty(shape=(2,10))
+        box_const[0,:] = -1; box_const[1,:] = 1
+        enet._validate_box_constraints(X, y, box_constraints=box_const)
